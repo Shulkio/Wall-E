e9a4100f87588b8b0ad51b09c8509a39135881ef
Tigrouw <lucas.francoise.mp@gmail.com>
2016-01-18 21:33:59 +0100

    ok c bon

--- Wall-E/src/Main/Main.java
+++ Wall-E/src/Main/Main.java
@@ -3,10 +3,7 @@ package Main;
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Lucasooo");
-=======
->>>>>>> branch 'master' of https://github.com/Shulkio/Wall-E.git
 	}
 
 }
